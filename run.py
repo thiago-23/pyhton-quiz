cba999173de70de31c30f032eb3e37383eebd945
thiago-23 <>
2024-02-12 22:02:28 +0000

    Add a display_question function with a sample Python Quiz question and a welcome message

--- run.py
+++ run.py
@@ -12,6 +12,11 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('python_quiz')
 
+print("Welcome to the Python Quiz Game!")
+print("Answer a series of Python-related questions and see how well you know the language.")
+print("For each question, choose the correct option (a, b, c, or d). Let's get started!\n")
+
+
 def get_quiz_data(question_title="questions"):
     """
     Retrieve quiz data and returns list of list representing questions with options.
@@ -26,7 +31,21 @@ def get_quiz_data(question_title="questions"):
         return []
 
 # Test the function
-quiz_data = get_quiz_data()
-print(quiz_data)
+# quiz_data = get_quiz_data()
+# print(quiz_data)
 
 
+def display_question(question_number, question_options):
+    """
+    Display a quiz question with options.
+
+    """
+    print(f"\nQuestion {question_number}: {question_options[0]}")
+    print(f"a) {question_options[1]}")
+    print(f"b) {question_options[2]}")
+    print(f"c) {question_options[3]}")
+    print(f"d) {question_options[4]}\n")
+
+# Test the function
+sample_question_options = ["What is 5 + 7?", "4", "5", "6", "12"]
+display_question(1, sample_question_options)
